7d0695c60bda071755d7bfd51c2a5ce87ee80ad6
cowtony <cowtony@163.com>
2020-03-07 10:26:41 -0800

    Create 10. Regular Expression Matching.cpp

---
+++ LeetCode/10. Regular Expression Matching.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        if (p.empty()) {
+            return s.empty();
+        }
+        
+        bool first_match = !s.empty() and (p.front() == s.front() or p.front() == '.');
+        
+        if (p.length() > 1 and p[1] == '*') {
+            if (first_match) {
+                return isMatch(s.substr(1), p) or isMatch(s, p.substr(2));
+            } else {
+                return isMatch(s, p.substr(2));
+            }
+        }
+        
+        return first_match and isMatch(s.substr(1), p.substr(1));
+    }
+};
