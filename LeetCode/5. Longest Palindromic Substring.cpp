d6151108bb2d50e761756be50a9f72b1275067ae
cowtony <cowtony@163.com>
2020-03-07 10:24:40 -0800

    Create 5. Longest Palindromic Substring.cpp

---
+++ LeetCode/5. Longest Palindromic Substring.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        if (s.empty()) {
+            return s;
+        }
+        int start, max_length = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int left = i - 1, right = i + 1;
+            while (left >= 0 and right < s.length()) {
+                if (s[left] != s[right]) {
+                    break;
+                }
+                left--;
+                right++;
+            }
+            if (max_length < right - left - 1) {
+                max_length = right - left - 1;
+                start = left + 1;
+            }
+            left = i;
+            right = i + 1;
+            while (left >= 0 and right < s.length()) {
+                if (s[left] != s[right]) {
+                    break;
+                }
+                left--;
+                right++;
+            }
+            if (max_length < right - left - 1) {
+                max_length = right - left - 1;
+                start = left + 1;
+            }
+        }
+        return s.substr(start, max_length);
+    }
+};
