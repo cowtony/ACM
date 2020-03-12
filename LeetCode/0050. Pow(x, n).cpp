aeb0ad50c6a444f831b9bb76a758a55136188142
cowtony <cowtony@163.com>
2020-03-11 20:58:55 -0700

    Create 0050. Pow(x, n).cpp

---
+++ LeetCode/0050. Pow(x, n).cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double result = 1;
+        if (n < 0) {
+            n = -n - 1;  // prevent n = -2^31
+            x = 1 / x;
+            result = x;
+        }
+        
+        double base = x;
+        while (n > 0) {
+            if (n & 1 == 1) {
+                result *= base;
+            }
+            base *= base;
+            n >>= 1;
+        }
+        
+        return result;
+    }
+};
