9d7847fc703a7bbb9117bd0c7ffb30c493fdb286
cowtony <cowtony@163.com>
2020-03-12 00:21:29 -0700

    Update 0063. Unique Paths II.cpp

--- LeetCode/0063. Unique Paths II.cpp
+++ LeetCode/0063. Unique Paths II.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        if (obstacleGrid.back().back() == 1 or obstacleGrid.front().front() == 1) {
-            return 0;
-        }
-        
-        vector<vector<long>> dp(obstacleGrid.size(), vector<long>(obstacleGrid[0].size(), 0));
-        dp[0][0] = 1;
-        for (int i = 0; i < obstacleGrid.size(); i++) {
-            for (int j = 0; j < obstacleGrid[i].size(); j++) {
-                if (j > 0 and obstacleGrid[i][j - 1] == 0) {
-                    dp[i][j] += dp[i][j - 1];
-                }
-                if (i > 0 and obstacleGrid[i - 1][j] == 0) {
-                    dp[i][j] += dp[i - 1][j];
+        vector<vector<long>> dp(obstacleGrid.size() + 1, vector<long>(obstacleGrid[0].size() + 1, 0));
+        dp[0][1] = 1;
+        for (int i = 1; i <= obstacleGrid.size(); i++) {
+            for (int j = 1; j <= obstacleGrid[0].size(); j++) {
+                if (obstacleGrid[i - 1][j - 1] == 0) {
+                    dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
                 }
             }
         }
-        return dp[obstacleGrid.size() - 1][obstacleGrid[0].size() - 1];
+        return dp[obstacleGrid.size()][obstacleGrid[0].size()];
     }
 };
