2cd45340b64245f97770554c3f2a3957978a6b8f
cowtony <cowtony@163.com>
2020-03-12 21:07:58 -0700

    Create 0066. Plus One.cpp

---
+++ LeetCode/0066. Plus One.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        digits[digits.size() - 1]++;
+        digits.insert(digits.begin(), 0);
+        for (int i = digits.size() - 1; i >= 1; i--)
+        {
+            if (digits[i] >= 10)
+            {
+                digits[i] -= 10;
+                digits[i - 1]++;
+            }
+            else break;
+        }
+        
+        if (digits[0] == 0)
+            digits.erase(digits.begin());
+        return digits;
+    }
+};
