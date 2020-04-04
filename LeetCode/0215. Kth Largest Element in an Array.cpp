c5e9f847d49e6513adf377113c2853a7341360b0
cowtony <cowtony@163.com>
2020-04-04 16:32:54 -0700

    Create 0215. Kth Largest Element in an Array.cpp

---
+++ LeetCode/0215. Kth Largest Element in an Array.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        // Convert k to start from 0 and smallest.
+        k = nums.size() - k;
+        
+        int lo = 0, hi = nums.size() - 1;
+        while (lo < hi) {
+            int curr = partition(nums, lo, hi);
+            if (curr > k) {
+                hi = curr - 1;
+            } else if (curr < k) {
+                lo = curr + 1;
+            } else {
+                return nums[curr];
+            }
+        }
+        return nums[lo];
+    }
+    
+    int partition(vector<int>& arr, int begin_idx, int end_idx)
+    {
+        int pivot = arr[begin_idx];
+        while(begin_idx < end_idx)
+        {
+            while(begin_idx < end_idx && arr[end_idx] >= pivot) {
+                --end_idx;
+            }
+            arr[begin_idx] = arr[end_idx];
+            
+            while(begin_idx < end_idx && arr[begin_idx] <= pivot) {
+                ++begin_idx;
+            }
+            arr[end_idx] = arr[begin_idx];
+        }
+        arr[begin_idx] = pivot;
+        return begin_idx;
+    }
+};
