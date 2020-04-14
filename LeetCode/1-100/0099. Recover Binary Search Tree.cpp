d229758de10851f2de02a6a004b435c65ae0e5e4
cowtony <cowtony@163.com>
2020-04-13 22:32:15 -0700

    Update 0099. Recover Binary Search Tree.cpp

--- LeetCode/1-100/0099. Recover Binary Search Tree.cpp
+++ LeetCode/1-100/0099. Recover Binary Search Tree.cpp
@@ -7,42 +7,45 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+void morrisTraversalInOrder(TreeNode* root, std::function<void(TreeNode*)> func) {
+    while (root) {  
+        if (!root->left) {  
+            func(root);
+            root = root->right;  
+        } else {  
+            TreeNode* pre = root->left;  
+            while (pre->right && pre->right != root) {
+                pre = pre->right;  
+            }  
+            if (!pre->right) { 
+                pre->right = root;
+                root = root->left;  
+            } else {  
+                pre->right = nullptr;  
+                func(root);
+                root = root->right;  
+            }  
+        }  
+    }
+}
+
 class Solution {
 public:
     void recoverTree(TreeNode* root) {
-        // Morris Traversal (preorder):
-        while (root) {  
-            if (!root->left) {  
-                action(root);
-                root = root->right;  
-            } else {  
-                TreeNode* current = root->left;  
-                while (current->right && current->right != root) {
-                    current = current->right;  
-                }  
-                if (current->right) { 
-                    action(root);
-                    current->right = nullptr;  
-                    root = root->right;  
-                } else {  
-                    current->right = root;
-                    root = root->left;  
-                }  
-            }  
-        }
-        swap(first->val, second->val);
-    }
-    
-    TreeNode* first = nullptr;
-    TreeNode* second = nullptr;
-    TreeNode* pre_node = nullptr;
-    void action(TreeNode* node) {
-        if (pre_node and pre_node->val > node->val) {
-            if (!first) {
-                first = pre_node;
+        TreeNode* first = nullptr;
+        TreeNode* second = nullptr;
+        TreeNode* pre_node = nullptr;
+        
+        morrisTraversalInOrder(root, [&first, &second, &pre_node](TreeNode* node){
+            if (pre_node and pre_node->val > node->val) {
+                if (!first) {
+                    first = pre_node;
+                }
+                second = node;
             }
-            second = node;
-        }
-        pre_node = node;
+            pre_node = node;
+        });
+        swap(first->val, second->val);
     }
 };
