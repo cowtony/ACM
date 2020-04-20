6a8d33a254b6a1323aad2b6b2eb3e5abdb0c8c63
cowtony <cowtony@163.com>
2020-04-19 21:06:50 -0700

    Update 0004. Median of Two Sorted Arrays.cpp

--- LeetCode/1-100/0004. Median of Two Sorted Arrays.cpp
+++ LeetCode/1-100/0004. Median of Two Sorted Arrays.cpp
@@ -1,73 +1,39 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> *shortNums, *longNums;
-        if (nums1.size() < nums2.size()) {
-            shortNums = &nums1;
-            longNums = &nums2;
+        int length = nums1.size() + nums2.size();
+        int k = length / 2;
+
+        if (length % 2 == 0) {
+            return (findKthNumber(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), k) + 
+                    findKthNumber(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), k + 1)) / 2.0;
+        } else {
+            return findKthNumber(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), k + 1);
         }
-        else {
-            shortNums = &nums2;
-            longNums = &nums1;
+    }
+    
+    int findKthNumber(const vector<int>::iterator& a_begin, 
+                      const vector<int>::iterator& a_end, 
+                      const vector<int>::iterator& b_begin, 
+                      const vector<int>::iterator& b_end, 
+                      int k) {
+        if (a_begin == a_end) {
+            return *(b_begin + k - 1);
+        }
+        if (b_begin == b_end) {
+            return *(a_begin + k - 1);
+        }
+        if (k == 1) {
+            return min(*a_begin, * b_begin);
         }
         
-        const int totalLen = nums1.size() + nums2.size();
-        int left = 0, right = shortNums->size();
-        int shortCut, longCut;
-        while (left <= right) {
-            shortCut = (left + right) / 2;
-            longCut = totalLen / 2 - shortCut;
-            cout << shortCut << ' ' << longCut << endl;
-            try {
-                if (shortNums->at(shortCut - 1) > longNums->at(longCut)) {
-                    right = shortCut - 1; // need to move shortCut left
-                    continue;
-                }
-            } catch (...) {}
-                
-            try {
-                if (longNums->at(longCut - 1) > shortNums->at(shortCut)) {
-                    left = shortCut + 1; // need to move shortCut right
-                    continue;
-                }
-            } catch (...) {}
-                
-                  
-            break;
-        }
-        cout << "Binary Search end" << endl;
+        int a_val = (a_end - a_begin < k / 2) ? INT_MAX : *(a_begin + k / 2 - 1);
+        int b_val = (b_end - b_begin < k / 2) ? INT_MAX : *(b_begin + k / 2 - 1);
         
-        if (totalLen % 2 == 0)
-        {
-            int left = -2100000000;
-            try {
-                left = max(left, shortNums->at(shortCut - 1));
-            } catch(...){}
-            try {
-                left = max(left, longNums->at(longCut - 1));
-            } catch(...){}
-            
-            int right = 2100000000;
-            try {
-                right = min(right, shortNums->at(shortCut));
-            } catch(...){}
-            try {
-                right = min(right, longNums->at(longCut));
-            } catch(...){}
-            cout << "Even:" << left << ' ' << right << endl;
-            return (left + right) / 2.0;
+        if (a_val < b_val) {
+            return findKthNumber(a_begin + k / 2, a_end, b_begin, b_end, k - k / 2);
+        } else {
+            return findKthNumber(a_begin, a_end, b_begin + k / 2, b_end, k - k / 2);
         }
-            
-        else {
-            int ret = 2100000000;
-            try {
-                ret = min(ret, shortNums->at(shortCut));
-            } catch(...){}
-            try {
-                ret = min(ret, longNums->at(longCut));
-            } catch(...){}
-            return ret;
-        }
-            
     }
 };
