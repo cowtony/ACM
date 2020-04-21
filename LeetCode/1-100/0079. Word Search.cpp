36c871fe7b7812e79637ea79d4ecd90794376fa9
cowtony <cowtony@163.com>
2020-04-20 18:38:10 -0700

    Update 0079. Word Search.cpp

--- LeetCode/1-100/0079. Word Search.cpp
+++ LeetCode/1-100/0079. Word Search.cpp
@@ -3,50 +3,43 @@ public:
     bool exist(vector<vector<char>>& board, string word) {
         for (int i = 0; i< board.size(); i++) {
             for (int j = 0; j < board[0].size(); j++) {
-                if (board[i][j] == word.front()) {
-                    board[i][j] = 0;
-                    if (dfs(board, i, j, word.substr(1))) {
-                        return true;
-                    }
-                    board[i][j] = word.front();
+                if (dfs(board, i, j, word)) {
+                    return true;
                 }
             }
         }
         return false;
     }
     
+    const vector<int> kX = {0, 0, 1, -1};
+    const vector<int> kY = {1, -1, 0, 0};
+    
     bool dfs(vector<vector<char>>& board, int row, int col, string word) {
         if (word.empty()) {
             return true;
         }
-        if (row > 0 and board[row - 1][col] == word.front()) {
-            board[row - 1][col] = 0;
-            if (dfs(board, row - 1, col, word.substr(1))) {
+        if (word.front() != board[row][col]) {
+            return false;
+        } else if (word.length() == 1) {
+            return true;
+        }
+        
+        board[row][col] = 0;
+        for (int i = 0; i < 4; i++) {
+            int r = row + kX.at(i);
+            int c = col + kY.at(i);
+            if (r < 0 or r >= board.size()) {
+                continue;
+            }
+            if (c < 0 or c >= board[0].size()) {
+                continue;
+            }
+            
+            if (dfs(board, r, c, word.substr(1))) {
                 return true;
             }
-            board[row - 1][col] = word.front();
         }
-        if (row + 1 < board.size() and board[row + 1][col] == word.front()) {
-            board[row + 1][col] = 0;
-            if (dfs(board, row + 1, col, word.substr(1))) {
-                return true;
-            }
-            board[row + 1][col] = word.front();
-        }
-        if (col > 0 and board[row][col - 1] == word.front()) {
-            board[row][col - 1] = 0;
-            if (dfs(board, row, col - 1, word.substr(1))) {
-                return true;
-            }
-            board[row][col - 1] = word.front();
-        }
-        if (col + 1 < board[0].size() and board[row][col + 1] == word.front()) {
-            board[row][col + 1] = 0;
-            if (dfs(board, row, col + 1, word.substr(1))) {
-                return true;
-            }
-            board[row][col + 1] = word.front();
-        }
-        return false;
+        board[row][col] = word.front();
+        return word.empty();
     }
 };
