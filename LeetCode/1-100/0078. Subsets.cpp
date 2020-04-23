3094f33bea791f42e47e424e5c6953b8ff41fdd1
cowtony <cowtony@163.com>
2020-04-22 23:48:48 -0700

    Update 0078. Subsets.cpp

--- LeetCode/1-100/0078. Subsets.cpp
+++ LeetCode/1-100/0078. Subsets.cpp
@@ -1,3 +1,22 @@
+// Iterative, DP
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> dp;
+        dp.push_back({});
+        
+        for (int i = 0; i < nums.size(); i++) {
+            vector<vector<int>> temp = dp;
+            for (vector<int>& array : temp) {
+                array.push_back(nums[i]);
+            }
+            dp.insert(dp.end(), temp.begin(), temp.end());
+        }
+        return dp;
+    }
+};
+
+/* Binary
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
@@ -16,3 +35,4 @@ public:
         return ret;
     }
 };
+*/
