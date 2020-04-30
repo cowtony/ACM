48e17b0e77fd42b5b3a006f572b95e057c75609e
cowtony <cowtony@163.com>
2020-04-30 13:27:47 -0700

    Update 0104. Maximum Depth of Binary Tree.cpp

--- LeetCode/101-200/0104. Maximum Depth of Binary Tree.cpp
+++ LeetCode/101-200/0104. Maximum Depth of Binary Tree.cpp
@@ -10,8 +10,9 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        if (root == NULL)
+        if (!root) {
             return 0;
-        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+        }
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
     }
 };
