ae612c3bdcfe8f5274495deebf9a6e9a2cd5b369
cowtony <cowtony@163.com>
2020-05-15 20:52:51 -0700

    Create 0880. Decoded String at Index.cpp

---
+++ LeetCode/0880. Decoded String at Index.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string decodeAtIndex(string S, int K) {
+        long len = 0;
+        int i = 0;
+        for (; i < S.length(); i++) {
+            if (len > K) {
+                break;
+            }
+            if (S[i] >= '0' and S[i] <= '9') {
+                len *= S[i] - '0';
+            } else {
+                len++;
+            }
+        }
+
+        for (i--; i >= 0; i--) {
+            if (S[i] >= '0' and S[i] <= '9') {
+                len /= S[i] - '0';
+                K %= len;
+            } else {
+                if (K == len or K == 0) {
+                    return string(1, S[i]);
+                }
+                len--;
+            }
+        }
+
+        return "";
+    }
+};
