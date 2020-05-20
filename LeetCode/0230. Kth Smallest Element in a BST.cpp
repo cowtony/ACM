768be5f56eb7c2c226c5a91ae8706363ad3243d4
cowtony <cowtony@163.com>
2020-05-20 10:41:30 -0700

    Update 0230. Kth Smallest Element in a BST.cpp

--- LeetCode/0230. Kth Smallest Element in a BST.cpp
+++ LeetCode/0230. Kth Smallest Element in a BST.cpp
@@ -8,24 +8,46 @@
  * };
  */
 
-void dfsInOrder(TreeNode* root, std::function<void(TreeNode*)> func) {
-    if (!root) { return; }
-    if (root->left) { dfsInOrder(root->left, func); }
-    func(root);
-    if (root->right) { dfsInOrder(root->right, func); }
+struct TreeNodeWithCount : public TreeNode {
+    int count = 1; // Store the order of this element.
+    int val;
+    TreeNodeWithCount *left = nullptr;
+    TreeNodeWithCount *right = nullptr;
+    TreeNodeWithCount(int x) : val(x) {}
+};
+
+TreeNodeWithCount* buildTreeWithCount(TreeNode* root) {
+    if (!root) {
+        return nullptr;
+    }
+    TreeNodeWithCount* new_root = new TreeNodeWithCount(root->val);
+    new_root->left = buildTreeWithCount(root->left);
+    if (new_root->left) {
+        new_root->count += new_root->left->count;
+    }
+    new_root->right = buildTreeWithCount(root->right);
+    if (new_root->right) {
+        new_root->count += new_root->right->count;
+    }
+    return new_root;
 }
 
 class Solution {
 public:
     int kthSmallest(TreeNode* root, int k) {
-        int ret;
-        int count = 0;
-        dfsInOrder(root, [&](TreeNode* node) { 
-            count++;
-            if (count == k) {
-                ret = node->val;
+        TreeNodeWithCount* node = buildTreeWithCount(root);
+
+        while (node) {
+            int left_count = node->left? node->left->count : 0;
+            if (k <= left_count) {
+                node = node->left;
+            } else if (k == left_count + 1) {
+                return node->val;
+            } else {
+                k -= left_count + 1;
+                node = node->right;
             }
-        });
-        return ret;
+        }
+        return -1;
     }
 };
