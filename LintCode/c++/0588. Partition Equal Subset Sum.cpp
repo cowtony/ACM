1be33806f785c76a9df51e65ce89f2e341006449
cowtony <cowtony@163.com>
2020-05-25 17:19:25 -0700

    Create 0588. Partition Equal Subset Sum.cpp

---
+++ LintCode/c++/0588. Partition Equal Subset Sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    /**
+     * @param nums: a non-empty array only positive integers
+     * @return: true if can partition or false
+     */
+    bool canPartition(vector<int> &nums) {
+        int target = 0;
+        for (int num : nums) {
+            target += num;
+        }
+
+        if (target % 2 == 1) {
+            return false;
+        }
+        target /= 2;
+        
+        vector<bool> dp(target + 1, false);
+        dp[0] = true;
+
+        for (int num : nums) {
+            for (int i = target; i >= num; i--) {
+                dp[i] = dp[i] or dp[i - num];
+            }
+        }
+
+        return dp[target];
+    }
+};
