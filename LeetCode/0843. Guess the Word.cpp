a1abd198301570857d8d5b0ce62e31c6f262e01a
cowtony <cowtony@163.com>
2020-06-15 13:20:05 -0700

    Update 0843. Guess the Word.cpp

--- LeetCode/0843. Guess the Word.cpp
+++ LeetCode/0843. Guess the Word.cpp
@@ -11,32 +11,13 @@ public:
     void findSecretWord(vector<string>& wordlist, Master& master) {
         int res = 0;
         while (res < 6) {
-            // 1. Count all the letter occurance at each position.
-            vector<vector<int>> count(6, vector<int>(26, 0));
-            for (const string& word : wordlist) {
-                for (int i = 0; i < 6; i++) {
-                    count[i][word[i] - 'a']++;
-                }
-            }
+            // 1. Get a candidate word to guess.
+            string candidate = getCandidate(wordlist);
             
-            // 2. Find a word with maximum overlap/count with others.
-            int max_score = -1;
-            string candidate;
-            for (const string& word : wordlist) {
-                int score = 0;
-                for (int i = 0; i < 6; i++) {
-                    score += count[i][word[i] - 'a'];
-                }
-                if (score > max_score) {
-                    max_score = score;
-                    candidate = word;
-                }
-            }
-            
-            // 3. Guess.
+            // 2. Guess.
             res = master.guess(candidate);
             
-            // 4. Delete all the words that matches with candidate different than result.
+            // 3. Delete all the words that have a different matches to candidate than result.
             vector<string> new_wordlist;
             for (const string& word : wordlist) {
                 int match = 0;
@@ -51,5 +32,31 @@ public:
             }
             wordlist = new_wordlist;
         }
+    }
+
+private:
+    // This is an optimize algorithm, ramdon pick will have more guesses.
+    string getCandidate(const vector<string>& wordlist) {
+        // 1. Count all the letter occurance at each position.
+        vector<vector<int>> count(6, vector<int>(26, 0));
+        for (const string& word : wordlist) {
+            for (int i = 0; i < 6; i++) {
+                count[i][word[i] - 'a']++;
+            }
+        }
+        // 2. Find a word with maximum overlap/count with others.
+        int max_score = -1;
+        string candidate;
+        for (const string& word : wordlist) {
+            int score = 0;
+            for (int i = 0; i < 6; i++) {
+                score += count[i][word[i] - 'a'];
+            }
+            if (score > max_score) {
+                max_score = score;
+                candidate = word;
+            }
+        }
+        return candidate;
     }
 };
