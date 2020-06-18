68f0e4bc2595c2718d2172eb52661c988d2066ec
cowtony <cowtony@163.com>
2020-06-17 19:41:05 -0700

    Create 0280. Wiggle Sort.cpp

---
+++ LeetCode/0280. Wiggle Sort.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        bool flip = true;
+        for (int i = 1; i < nums.size(); i++) {
+            if (flip) {
+                if (nums[i] < nums[i - 1]) {
+                    swap(nums[i], nums[i - 1]);
+                }
+            } else {
+                if (nums[i] > nums[i - 1]) {
+                    swap(nums[i], nums[i - 1]);
+                }
+            }
+            flip = !flip;
+        }
+    }
+};
