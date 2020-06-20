3ecd1c91ab35e9ece5b0b5268684d02ea7ccd5d0
cowtony <cowtony@163.com>
2020-06-19 21:22:25 -0700

    Update graph.cpp

--- Notes/graph.cpp
+++ Notes/graph.cpp
@@ -1,9 +1,9 @@
 // Floyd Warshall O(V^3)
-void floyd(vector<vector<int>>& adjacency) {
+void floyd(vector<vector<int>>& adjacency_matrix) {
     for(int k = 0; k < 26; k ++) {
         for(int i = 0; i < 26; i ++) {
             for(int j = 0; j < 26; j ++) {
-                adjacency[i][j] = min(adjacency[i][j], adjacency[i][k] + adjacency[k][j]);
+                adjacency_matrix[i][j] = min(adjacency_matrix[i][j], adjacency_matrix[i][k] + adjacency_matrix[k][j]);
             }
         }
     }
@@ -11,29 +11,47 @@ void floyd(vector<vector<int>>& adjacency) {
 
 // Dijkstra O((E+V)log(V))
 int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
-        vector<vector<pair<int, int>>> graph(n); // Next node and cost.
-        for (auto edge : flights) {
-            graph[edge[0]].emplace_back(edge[1], edge[2]);
-        }
-        
-        typedef tuple<int, int, int> Node; // distance, vertex, edges.
-        priority_queue<Node, vector<Node>, greater<Node>> pq;
-        pq.emplace(0, src, 0);
-        while (!pq.empty()) {
-            auto [distance, vertex, stops] = pq.top();
-            pq.pop();
-            
-            if(vertex == dst) {
-                return distance;
-            }
-            if(stops > K) {
-                continue;
-            }
-            for(auto edge : graph[vertex]) {
-                auto [next, weight] = edge;
-                pq.emplace(distance + weight, next, stops + 1);
-            }
-        }
-        
-        return -1;
+    vector<vector<pair<int, int>>> graph(n); // Next node and cost.
+    for (auto edge : flights) {
+        graph[edge[0]].emplace_back(edge[1], edge[2]);
     }
+
+    typedef tuple<int, int, int> Node; // distance, vertex, edges.
+    priority_queue<Node, vector<Node>, greater<Node>> pq;
+    pq.emplace(0, src, 0);
+    while (!pq.empty()) {
+        auto [distance, vertex, stops] = pq.top();
+        pq.pop();
+
+        if(vertex == dst) {
+            return distance;
+        }
+        if(stops > K) {
+            continue;
+        }
+        for(auto edge : graph[vertex]) {
+            auto [next, weight] = edge;
+            pq.emplace(distance + weight, next, stops + 1);
+        }
+    }
+
+    return -1;
+}
+
+// Check if graph has cycle
+bool hasCycle(const vector<vector<int>>& adjacency_list, vector<bool>& visited, int node = 0) {
+    if (node < 0 or node >= visited.size()) {
+        return false;
+    }
+    if (visited[node]) {
+        return true;
+    }
+    visited[node] = true;
+    for (int next : adjacency_list.at(node)) {
+        if (hasCycle(adjacency_list, visited, next)) {
+            return true;
+        }
+    }
+    visited[node] = false;
+    return false;
+}
