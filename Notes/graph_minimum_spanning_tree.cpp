0f8a267ded84ec61c47e2f4f450a550ff0b95843
cowtony <cowtony@163.com>
2020-06-20 22:38:27 -0700

    Update graph_minimum_spanning_tree.cpp

--- Notes/graph_minimum_spanning_tree.cpp
+++ Notes/graph_minimum_spanning_tree.cpp
@@ -1,1 +1,16 @@
+#include "union_find.h"
 
+int minimumSpanningTree(vector<vector<int>>& edges) {
+    sort(edges.begin(), edges.end(), [](const vector<int>& a, const vector<int>& b) {
+        return a[2] < b[2];
+    });
+    int weight = 0;
+    UnionFind<int> uf;
+    for (const auto& edge : edges) {
+        if (uf.root(edge[0]) != uf.root(edge[1])) {
+            weight += edge[2];
+            uf.connect(edge[0], edge[1]);
+        }
+    }
+    return weight;
+}
