f7ae70cba1b80f9609021c7abbc9347325ab32c8
cowtony <cowtony@163.com>
2020-06-24 11:53:13 -0700

    Update 0106. Construct Binary Tree from Inorder and Postorder Traversal.cpp

--- LeetCode/101-200/0106. Construct Binary Tree from Inorder and Postorder Traversal.cpp
+++ LeetCode/101-200/0106. Construct Binary Tree from Inorder and Postorder Traversal.cpp
@@ -4,24 +4,32 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        return buildTreeRecursive(inorder, postorder, 0, 0, inorder.size());
+        return buildTree(inorder.begin(), inorder.end(), postorder.begin(), postorder.end());
     }
     
-    TreeNode* buildTreeRecursive(vector<int>& inorder, 
-                                 vector<int>& postorder, 
-                                 int in_idx, int post_idx, int length) {
-        for (int i = 0; i < length; i++) {
-            if (inorder[in_idx + i] == postorder[post_idx + length - 1]) {
-                TreeNode *root = new TreeNode(inorder[in_idx + i]);
-                root->left = buildTreeRecursive(inorder, postorder, in_idx, post_idx, i);
-                root->right = buildTreeRecursive(inorder, postorder, in_idx + i + 1, post_idx + i, length - i - 1);
-                return root;
+    TreeNode* buildTree(vector<int>::const_iterator inorder_begin, 
+                        vector<int>::const_iterator inorder_end, 
+                        vector<int>::const_iterator postorder_begin,
+                        vector<int>::const_iterator postorder_end) {
+        if (inorder_begin == inorder_end or postorder_begin == postorder_end) {
+            return nullptr;
+        }
+        
+        int root_val = *(postorder_end - 1);
+        int i = 0;
+        for (auto it = inorder_begin; it != inorder_end; it++, i++) {
+            if (*it == root_val) {
+                return new TreeNode(root_val, 
+                                    buildTree(inorder_begin, inorder_begin + i, postorder_begin, postorder_begin + i),
+                                    buildTree(inorder_begin + i + 1, inorder_end, postorder_begin + i, postorder_end - 1));
             }
         }
         return nullptr;
