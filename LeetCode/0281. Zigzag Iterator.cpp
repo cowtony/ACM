202c118d1ef8dc8abafda052e656f7e0a6e0b6f8
cowtony <cowtony@163.com>
2020-06-24 18:44:02 -0700

    Update 0281. Zigzag Iterator.cpp

--- LeetCode/0281. Zigzag Iterator.cpp
+++ LeetCode/0281. Zigzag Iterator.cpp
@@ -2,36 +2,30 @@ class ZigzagIterator {
 public:
     ZigzagIterator(vector<int>& v1, vector<int>& v2) {
         if (v1.begin() != v1.end()) {
-            begins.push(v1.begin());
-            ends.push(v1.end());
+            vectors.push({v1.begin(), v1.end()});
         }
         if (v2.begin() != v2.end()) {
-            begins.push(v2.begin());
-            ends.push(v2.end());
+            vectors.push({v2.begin(), v2.end()});
         }
     }
 
     int next() {
-        auto begin = begins.front();
-        begins.pop();
-        auto end = ends.front();
-        ends.pop();
+        auto [begin, end] = vectors.front();
+        vectors.pop();
         int val = *begin;
         ++begin;
         if (begin != end) {
-            begins.push(begin);
-            ends.push(end);
+            vectors.push({begin, end});
         }
         return val;
     }
 
     bool hasNext() {
-        return !begins.empty();
+        return !vectors.empty();
     }
     
 private:
-    queue<vector<int>::const_iterator> begins;
-    queue<vector<int>::const_iterator> ends;
+    queue<pair<vector<int>::const_iterator, vector<int>::const_iterator>> vectors; // begin, end
 };
 
 /**
