806d4c639cb5b400f5cf490f008b81fce63ea49d
cowtony <cowtony@163.com>
2020-06-26 22:35:58 -0700

    Create 0257. Longest String Chain.cpp

---
+++ LintCode/c++/0257. Longest String Chain.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    /**
+     * @param words: the list of word.
+     * @return: the length of the longest string chain.
+     */
+    int longestStrChain(vector<string> &words) {
+        if (words.empty()) {
+            return 0;
+        }
+        vector<unordered_set<string>> words_len(17);
+        unordered_map<string, int> chain;
+        for (const string& word : words) {
+            words_len[word.length()].insert(word);
+            chain[word] = 1;
+        }
+        
+        int max_chain = 1;
+        for (int len = 16; len > 0; len--) {
+            for (const string& word : words_len[len]) {
+                for (int i = 0; i < word.length(); i++) {
+                    string predecessor = word.substr(0, i) + word.substr(i + 1);
+                    if (chain.find(predecessor) != chain.end()) {
+                        chain[predecessor] = max(chain[predecessor], chain[word] + 1);
+                        max_chain = max(max_chain, chain[predecessor]);
+                    }
+                }
+            }
+        }
+        
+        return max_chain;
+    }
+};
