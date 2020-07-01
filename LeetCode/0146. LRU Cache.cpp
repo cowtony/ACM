6b9b59436db8dc52ce5d2f52a1e96a910834e715
cowtony <cowtony@163.com>
2020-06-30 21:12:18 -0700

    Update 0146. LRU Cache.cpp

--- LeetCode/0146. LRU Cache.cpp
+++ LeetCode/0146. LRU Cache.cpp
@@ -8,30 +8,39 @@ public:
         if (data.find(key) == data.end()) {
             return -1;
         }
-        auto it = data.at(key).first;
-        frequent.push_front(*it);
-        frequent.erase(it);
-        data[key].first = frequent.begin();
-        return data.at(key).second;
+        update(key);
+        return data[key].second;
     }
     
     void put(int key, int value) {
         if (data.find(key) != data.end()) {
-            get(key);
-            data[key].second = value;
-            return;
+            update(key);
+        } else {
+            frequent.push_front(key);
         }
-        if (frequent.size() == max_size) {
-            int key = frequent.back();
-            data.erase(key);
+        data[key] = make_pair(frequent.begin(), value);
+        if (frequent.size() > max_size) {
+            data.erase(frequent.back());
             frequent.pop_back();
         }
-        frequent.push_front(key);
-        data[key] = make_pair(frequent.begin(), value);
     }
     
 private:
     int max_size;
     unordered_map<int, pair<list<int>::iterator, int>> data; // <key, <iterator, value>>
     list<int> frequent; // Store keys as doublely linked list.
+    
+    void update(int key) {
+        auto it = data.at(key).first;
+        frequent.push_front(*it);
+        frequent.erase(it);
+        data[key].first = frequent.begin();
+    }
 };
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
