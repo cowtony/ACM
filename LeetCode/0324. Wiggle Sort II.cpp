2a773e413608138cde0d4241a1f97b8b005a47df
cowtony <cowtony@163.com>
2020-07-03 09:52:55 -0700

    Update 0324. Wiggle Sort II.cpp

--- LeetCode/0324. Wiggle Sort II.cpp
+++ LeetCode/0324. Wiggle Sort II.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
         // Partition sort by median.
-        // auto midptr = nums.begin() + nums.size() / 2;
-        // nth_element(nums.begin(), midptr, nums.end());
-        // int median = *midptr;
-        int median = findKthElement(nums, nums.size() / 2);
+        auto midptr = nums.begin() + nums.size() / 2;
+        nth_element(nums.begin(), midptr, nums.end());
+        int median = *midptr;
         
         // Index-rewiring.
         #define A(i) nums[(1 + 2 * (i)) % (nums.size() | 1)]
@@ -26,39 +25,5 @@ public:
                 mid++;
             }
         }
-    }
-    
-    int findKthElement(vector<int>& nums, int k) {        
-        int lo = 0, hi = nums.size() - 1;
-        while (lo < hi) {
-            int curr = partition(nums, lo, hi);
-            if (curr > k) {
-                hi = curr - 1;
-            } else if (curr < k) {
-                lo = curr + 1;
-            } else {
-                return nums[curr];
-            }
-        }
-        return nums[lo];
-    }
-    
-    int partition(vector<int>& arr, int begin_idx, int end_idx)
-    {
-        int pivot = arr[begin_idx];
-        while(begin_idx < end_idx)
-        {
-            while(begin_idx < end_idx && arr[end_idx] >= pivot) {
-                --end_idx;
-            }
-            arr[begin_idx] = arr[end_idx];
-            
-            while(begin_idx < end_idx && arr[begin_idx] <= pivot) {
-                ++begin_idx;
-            }
-            arr[end_idx] = arr[begin_idx];
-        }
-        arr[begin_idx] = pivot;
-        return begin_idx;
     }
 };
