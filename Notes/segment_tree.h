e3a964edfcf865a6b4a63bba44e8ea96fbfee45b
cowtony <cowtony@163.com>
2020-07-05 10:52:52 -0700

    Update segment_tree.h

--- Notes/segment_tree.h
+++ Notes/segment_tree.h
@@ -4,55 +4,55 @@ public:
     SegmentTree(int start, int end, T data = T()) {
         root = build(start, end, data);
     }
-    void update(int a, T data = T()) { update(root, a, data); }
+    void update(int a, T delta = T()) { update(root, a, delta); }
     T query(int start, int end) { return query(root, start, end); }
     
 private:
-    struct Node {
+    struct SegmentTreeNode {
         int start, end;
         T data; // min, max, count, etc.
-        Node *left = nullptr, *right = nullptr;
-    }*root;
+        SegmentTreeNode *left = nullptr, *right = nullptr;
+    };
+    SegmentTreeNode* root;
     
-    static Node* build(int start, int end, T default_data) {
+    static SegmentTreeNode* build(int start, int end, T default_value) {
         if (start > end) {
             return nullptr;
         }
-        Node *root = new Node{start, end, default_data};
+        SegmentTreeNode *root = new SegmentTreeNode{start, end, default_value};
         if (start < end) {
-            root->left = build(start, (start + end) / 2, default_data);
-            root->right = build((start + end) / 2 + 1, end, default_data);
+            root->left = build(start, (start + end) / 2, default_value);
+            root->right = build((start + end) / 2 + 1, end, default_value);
         }
         return root;
     }
     
-    static void update(Node* root, int pos, T data) {
-        if (!root or root->start > pos or root->end < pos) { return; }
-
-        root->data += data;  // Modify this.
+    static void update(SegmentTreeNode* root, int pos, T delta) {
+        if (!root) { return; }
+        if (pos < root->start or pos > root->end) {
+            return;
+        }
+        root->data += delta;  // Sum Tree.
         if (!root->left and !root->right) {
             return;
         }
         if (pos <= root->left->end) {
-            update(root->left, pos, data);
+            update(root->left, pos, delta);
         } else if (pos >= root->right->start) {
-            update(root->right, pos, data);
+            update(root->right, pos, delta);
         }
     }
     
-    static T query(Node* root, int start, int end) {
+    static T query(SegmentTreeNode* root, int start, int end) {
         if (!root) { return T(); }
         if (start <= root->start and end >= root->end) {
             return root->data;
         }
         if (start > root->end or end < root->start) {
-            return T(); // sum.
+            return T(); // Sum Tree.
         }
         T left = query(root->left, start, end);
         T right = query(root->right, start, end);
-        
-        return left + right; // sum range.
-        // return max(left, right); // get max.
-        // return min(left, right); // get min.
+        return left + right; // Sum Tree.
     }
 };
