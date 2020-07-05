b4795adaf10936dcd326be89582e11c977a87695
cowtony <cowtony@163.com>
2020-07-05 10:52:08 -0700

    Update 0315. Count of Smaller Numbers After Self.cpp

--- LeetCode/0315. Count of Smaller Numbers After Self.cpp
+++ LeetCode/0315. Count of Smaller Numbers After Self.cpp
@@ -1,10 +1,10 @@
-template<class T>
+template<class T = int>
 class SegmentTree {
 public:
     SegmentTree(int start, int end, T data = T()) {
         root = build(start, end, data);
     }
-    void update(int a, T data = T()) { update(root, a, data); }
+    void update(int a, T delta = T()) { update(root, a, delta); }
     T query(int start, int end) { return query(root, start, end); }
     
 private:
@@ -27,19 +27,19 @@ private:
         return root;
     }
     
-    static void update(SegmentTreeNode* root, int pos, T data) {
+    static void update(SegmentTreeNode* root, int pos, T delta) {
         if (!root) { return; }
         if (pos < root->start or pos > root->end) {
             return;
         }
-        root->data++;  // Modify this.
+        root->data += delta;  // Sum Tree.
         if (!root->left and !root->right) {
             return;
         }
         if (pos <= root->left->end) {
-            update(root->left, pos, data);
+            update(root->left, pos, delta);
         } else if (pos >= root->right->start) {
-            update(root->right, pos, data);
+            update(root->right, pos, delta);
         }
     }
     
@@ -49,11 +49,11 @@ private:
             return root->data;
         }
         if (start > root->end or end < root->start) {
-            return 0; // Modify this.
+            return T(); // Sum Tree.
         }
         T left = query(root->left, start, end);
         T right = query(root->right, start, end);
-        return left + right; // Modify this.
+        return left + right; // Sum Tree.
     }
 };
 
@@ -61,6 +61,21 @@ class Solution {
 public:
     vector<int> countSmaller(vector<int>& nums) {
         // Discretization, make the large min-max range to unique limited number.
+        int range = discretization(nums);
+        
+        // Segment tree solution:
+        SegmentTree<> tree(0, range);  // Store each unique value's occurance.
+        vector<int> res;
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            res.push_back(tree.query(0, nums[i] - 1));
+            tree.update(nums[i], 1);
+        }
+        
+        reverse(res.begin(), res.end());
+        return res;
+    }
+    
+    int discretization(vector<int>& nums) {
         vector<int> discrte = nums;
         sort(discrte.begin(), discrte.end());
         int unique = 0;
@@ -73,15 +88,6 @@ public:
         for (int& num : nums) {
             num = m.at(num);
         }
-        
-        // Segment tree solution:
-        SegmentTree<int> tree(0, unique);
-        vector<int> res;
-        for (int i = nums.size() - 1; i >= 0; i--) {
-            res.push_back(tree.query(INT_MIN, nums[i] - 1));
-            tree.update(nums[i]);
-        }
-        reverse(res.begin(), res.end());
-        return res;
+        return unique;
     }
 };
