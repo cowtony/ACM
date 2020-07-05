c33e80bafc8b73595efba800f048a961dacad063
cowtony <cowtony@163.com>
2020-07-05 00:24:53 -0700

    Update segment_tree.h

--- Notes/segment_tree.h
+++ Notes/segment_tree.h
@@ -1,4 +1,4 @@
-template<class T>
+template<class T = int>
 class SegmentTree {
 public:
     SegmentTree(int start, int end, T data = T()) {
@@ -8,31 +8,28 @@ public:
     T query(int start, int end) { return query(root, start, end); }
     
 private:
-    struct SegmentTreeNode {
+    struct Node {
         int start, end;
         T data; // min, max, count, etc.
-        SegmentTreeNode *left = nullptr, *right = nullptr;
-    };
-    SegmentTreeNode* root;
+        Node *left = nullptr, *right = nullptr;
+    }*root;
     
-    static SegmentTreeNode* build(int start, int end, T default_value) {
+    static Node* build(int start, int end, T default_data) {
         if (start > end) {
             return nullptr;
         }
-        SegmentTreeNode *root = new SegmentTreeNode{start, end, default_value};
+        Node *root = new Node{start, end, default_data};
         if (start < end) {
-            root->left = build(start, (start + end) / 2, default_value);
-            root->right = build((start + end) / 2 + 1, end, default_value);
+            root->left = build(start, (start + end) / 2, default_data);
+            root->right = build((start + end) / 2 + 1, end, default_data);
         }
         return root;
     }
     
-    static void update(SegmentTreeNode* root, int pos, T data) {
-        if (!root) { return; }
-        if (pos < root->start or pos > root->end) {
-            return;
-        }
-        root->data++;  // Modify this.
+    static void update(Node* root, int pos, T data) {
+        if (!root or root->start > pos or root->end < pos) { return; }
+
+        root->data += data;  // Modify this.
         if (!root->left and !root->right) {
             return;
         }
@@ -43,16 +40,19 @@ private:
         }
     }
     
-    static T query(SegmentTreeNode* root, int start, int end) {
+    static T query(Node* root, int start, int end) {
         if (!root) { return T(); }
         if (start <= root->start and end >= root->end) {
             return root->data;
         }
         if (start > root->end or end < root->start) {
-            return 0; // Modify this.
+            return T(); // sum.
         }
         T left = query(root->left, start, end);
         T right = query(root->right, start, end);
-        return left + right; // Modify this.
+        
+        return left + right; // sum range.
+        // return max(left, right); // get max.
+        // return min(left, right); // get min.
     }
 };
