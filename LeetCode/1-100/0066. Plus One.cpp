2947526a583c07176a03dbfc66bda7a749934830
cowtony <cowtony@163.com>
2020-07-06 09:47:37 -0700

    Update 0066. Plus One.cpp

--- LeetCode/1-100/0066. Plus One.cpp
+++ LeetCode/1-100/0066. Plus One.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        digits[digits.size() - 1]++;
-        digits.insert(digits.begin(), 0);
-        for (int i = digits.size() - 1; i >= 1; i--)
-        {
-            if (digits[i] >= 10)
-            {
-                digits[i] -= 10;
-                digits[i - 1]++;
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            if (digits[i] == 9) {
+                digits[i] = 0;
+            } else {
+                digits[i]++;
+                return digits;
             }
-            else break;
         }
         
-        if (digits[0] == 0)
-            digits.erase(digits.begin());
+        // 99999 + 1 = 100000
+        digits[0] = 1;
+        digits.push_back(0);
         return digits;
     }
 };
