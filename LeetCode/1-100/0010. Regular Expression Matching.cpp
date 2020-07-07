37b013aeb60cfd40eaa53102187895b097fba807
cowtony <cowtony@163.com>
2020-07-07 00:06:20 -0700

    Update 0010. Regular Expression Matching.cpp

--- LeetCode/1-100/0010. Regular Expression Matching.cpp
+++ LeetCode/1-100/0010. Regular Expression Matching.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        if (p.empty()) {
-            return s.empty();
-        }
+        // s\p  0 c * a * b
+        //  0   T F T F T F
+        //  a   F F F T T F
+        //  a   F F F F T F
+        //  b   F F F F F T
         
-        bool first_match = !s.empty() and (p.front() == s.front() or p.front() == '.');
+        vector<vector<bool>> dp(s.length() + 1, vector<bool>(p.length() + 1, false));
+        dp[0][0] = true;
         
-        if (p.length() > 1 and p[1] == '*') {
-            if (first_match) {
-                return isMatch(s.substr(1), p) or isMatch(s, p.substr(2));
-            } else {
-                return isMatch(s, p.substr(2));
+        for (int i = 0; i <= s.length(); i++) {
+            for (int j = 1; j <= p.length(); j++) {
+                
+                if (i > 0 and (p[j - 1] == s[i - 1] or p[j - 1] == '.')) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                
+                if (p[j - 1] == '*') { 
+                    dp[i][j] = dp[i][j - 2]; // j - 2 is garenteed to be valid since p cannot start with '*'.
+                    dp[i][j] = dp[i][j] or i > 0 and dp[i - 1][j] and (s[i - 1] == p[j - 2] or p[j - 2] == '.');
+                }
             }
         }
         
-        return first_match and isMatch(s.substr(1), p.substr(1));
+        return dp[s.length()][p.length()];
     }
 };
