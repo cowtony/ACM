4cc8dee425aefebd9186c323d3a3370308d26f67
cowtony <cowtony@163.com>
2020-07-07 21:42:54 -0700

    Create 0311. Sparse Matrix Multiplication.cpp

---
+++ LeetCode/0311. Sparse Matrix Multiplication.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> multiply(vector<vector<int>>& A, vector<vector<int>>& B) {
+        map<int, vector<pair<int, int>>> rowA; // Row, Col, Val
+        for (int r = 0; r < A.size(); r++) {
+            for (int c = 0; c < A[0].size(); c++) {
+                if (A[r][c] != 0) {
+                    rowA[r].push_back(make_pair(c, A[r][c]));
+                }
+            }
+        }
+        map<int, map<int, int>> colB;  // Col, Row, Val
+        for (int r = 0; r < B.size(); r++) {
+            for (int c = 0; c < B[0].size(); c++) {
+                if (B[r][c] != 0) {
+                    colB[c][r] = B[r][c];
+                }
+            }
+        }
+        
+        vector<vector<int>> ret(A.size(), vector<int>(B[0].size(), 0));
+        for (int r = 0; r < A.size(); r++) {
+            for (int c = 0; c < B[0].size(); c++) {
+                for (auto p : rowA[r]) {
+                    ret[r][c] += p.second * colB[c][p.first];
+                }
+            }
+        }
+        return ret;
+    }
+};
