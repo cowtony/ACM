73cd017e9568d820eb190061c5fc762dd25f6d77
cowtony <cowtony@163.com>
2020-07-23 15:47:56 -0700

    Update 0260. Single Number III.cpp

--- LeetCode/0260. Single Number III.cpp
+++ LeetCode/0260. Single Number III.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
+        // get num_1 ^ num_2:
         int diff = 0;
-        for (int num : nums)
-        {
+        for (int num : nums) {
             diff ^= num;
         }
-
+        
         diff &= -diff;   // get LSB bit 1
-
+        
         vector <int> result = {0, 0};
-        for (int num : nums)
-        {
-            if ((num & diff) == 0)
-            {
+        for (int num : nums) {
+            if ((num & diff) == 0) {
                 result[0] ^= num;
-            }
-            else
+            } else {
                 result[1] ^= num;
+            }
         }
         return result;
     }
