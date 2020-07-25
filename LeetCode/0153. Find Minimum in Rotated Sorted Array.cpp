56dd27d13a1c3da3f8b20ce2397aad4d5d0b93d5
cowtony <cowtony@163.com>
2020-07-25 07:27:12 -0700

    Create 0153. Find Minimum in Rotated Sorted Array.cpp

---
+++ LeetCode/0153. Find Minimum in Rotated Sorted Array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int lo = 0, hi = nums.size() - 1;
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+            if (nums[mid] > nums[hi]) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+        return nums[lo];
+    }
+};
