c1fb35d478fff74164d288914f71da2dfb252899
cowtony <cowtony@163.com>
2020-07-31 08:19:22 -0700

    Update 0070. Climbing Stairs.cpp

--- LeetCode/1-100/0070. Climbing Stairs.cpp
+++ LeetCode/1-100/0070. Climbing Stairs.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> DP(n + 1, 0);
-        DP[0] = 1;
-        DP[1] = 2;
-        for (int i = 2; i < n; i++)
-            DP[i] = DP[i - 1] + DP[i - 2];
-        
-        return DP[n - 1];
+        vector<int> dp(n + 1, 0);
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 };
