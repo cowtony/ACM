ddf156eceaa975ca3e1fc8216c24773400514709
cowtony <cowtony@163.com>
2020-08-01 21:17:29 -0700

    Create 1534. Count Good Triplets.cpp

---
+++ LeetCode/1501-1600/1534. Count Good Triplets.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+        int res = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            for (int j = i + 1; j < arr.size(); j++) {
+                if (abs(arr[i] - arr[j]) > a) {
+                    continue;
+                }
+                for (int k = j + 1; k < arr.size(); k++) {
+                    if (abs(arr[i] - arr[k]) <= c and abs(arr[j] - arr[k]) <= b) {
+                        res++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
