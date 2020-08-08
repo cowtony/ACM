9e8e28a3e98c221a52318b6a9d23d8ea10dfe56f
cowtony <cowtony@163.com>
2020-08-08 13:56:33 -0700

    Create 1539. Kth Missing Positive Number.cpp

---
+++ LeetCode/1501-1600/1539. Kth Missing Positive Number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int lo = 0, hi = arr.size();
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+            if (arr[mid] - (mid + 1) >= k) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return k + lo;
+    }
+};
