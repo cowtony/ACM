84644f889b28c25f0bb8f96ff3b0073caa78f3db
cowtony <cowtony@163.com>
2020-08-08 13:01:23 -0700

    Update bit_manipulation.h

--- Notes/bit_manipulation.h
+++ Notes/bit_manipulation.h
@@ -1,8 +1,22 @@
 int bitCount(int n) {
-      int res = 0;
-      while (n) {
-          res++;
-          n &= n - 1; 
-      }
-      return res;
+    int res = 0;
+    while (n) {
+        res++;
+        n &= n - 1; 
+    }
+    return res;
 }
+
+void setBit(int& num, int k) {
+    num |= 1UL << k;
+}
+
+void clearBit(int& num, int k) {
+    num &= ~(1UL << k);
+}
+
+void toggleBit(int& num, int k) {
+    num ^= 1UL << k;
+}
+
+std::bitset<10> x;
