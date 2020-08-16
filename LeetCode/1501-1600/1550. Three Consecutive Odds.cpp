879f20e0d6c5d13d16ed467dc1a8212c249ad78b
cowtony <cowtony@163.com>
2020-08-15 21:24:56 -0700

    Create 1550. Three Consecutive Odds.cpp

---
+++ LeetCode/1501-1600/1550. Three Consecutive Odds.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int count = 0;
+        for (int num : arr) {
+            if (num % 2 == 1) {
+                if (++count == 3) {
+                    return true;
+                }
+            } else {
+                count = 0;
+            }
+        }
+        return false;
+    }
+};
