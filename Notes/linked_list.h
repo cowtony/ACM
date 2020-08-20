9fbad0f76cb8509e94692ae3c373b5edb42c44b7
cowtony <cowtony@163.com>
2020-08-20 00:55:19 -0700

    Create linked_list.h

---
+++ Notes/linked_list.h
@@ -0,0 +1,19 @@
+// Reverse
+ListNode* reverseList(ListNode* head) {
+    ListNode *left = nullptr, *right = head;
+    while (right) {
+        ListNode* nextRight = right->next;
+        right->next = left;  // flip
+        left = right;
+        right = nextRight;
+    }
+    return left;
+}
+
+void mergeList(ListNode* head1, ListNode* head2) {
+    while (head1 and head2) {
+        ListNode* temp = head1->next;
+        head1 = head1->next = head2;
+        head2 = temp;
+    }
+}
