9b551d806655111257253f85076990186e5ee598
cowtony <cowtony@163.com>
2021-05-07 13:04:33 -0700

    Update trie.h

--- Notes/trie.h
+++ Notes/trie.h
@@ -50,3 +50,81 @@ public:
         }
     }*root;
 };
+
+template<class T = string>
+class Trie {
+  public:
+    Trie() : children(26, nullptr) {}
+    ~Trie() {
+        for (Trie* child : children) {
+            delete child;
+        }
+    }
+    
+    void insert(const string& word, const T data = T()) {
+        Trie* node = this;
+        for (const char c : word) {
+            if (!node->children.at(c - 'a')) {
+                node->children[c - 'a'] = new Trie;
+            }
+            node = node->children.at(c - 'a');
+        }
+        node->data = data;  // Store data here.
+        node->word = word;
+    }
+    
+    T get(const string& word) const {
+        if (Trie* node = getNode(word)) {
+            return node->data;
+        }
+        return T();
+    }
+    
+    vector<T> getAllPrefix(const string& word) const {
+        vector<T> res;
+        Trie* node = this;
+        for (const char c : word.substr(0, word.length() - 1)) {
+            if (!(node = node->children.at(c - 'a'))) {
+                return res;
+            }
+            if (!node->data.empty()) {
+                res.push_back(node->data);
+            }
+        }
+        return res;
+    }
+    
+    vector<T> getAllData(const string& prefix = "") const {
+        const Trie* node = getNode(prefix);
+        if (!node) {
+            return {};
+        }
+        // DFS to collect all data.
+        vector<T> res;
+        if (!node->word.empty()) {
+            res.push_back(node->data);
+        }
+        for (Trie* child : node->children) {
+            if (child) {
+                vector<T> val = child->getAllData();
+                res.insert(res.end(), val.begin(), val.end());
+            }
+        }
+        return res;
+    }
+    
+  private:
+    vector<Trie*> children;
+    string word;
+    T data = T();
+    
+    const Trie* getNode(const string& prefix) const {
+        const Trie* node = this;
+        for (const char c : prefix) {
+            if (!(node = node->children.at(c - 'a'))) {
+                return nullptr;
+            }
+        }
+        return node;
+    }
+};
