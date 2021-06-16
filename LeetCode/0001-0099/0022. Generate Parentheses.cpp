548dbb9acb89d324f65c77aabbc40dcfbcb473d4
cowtony <cowtony@163.com>
2021-06-16 10:01:40 -0700

    Update 0022. Generate Parentheses.cpp

--- LeetCode/0001-0099/0022. Generate Parentheses.cpp
+++ LeetCode/0001-0099/0022. Generate Parentheses.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-    int number;
-    vector<string> result;
-    
-    void DFS(string s, int stack)
-    {
-        if (s.length() == number * 2)
-        {
-            if (stack == 0)
-                result.push_back(s);
-            return;
-        }
-        if (stack < number)
-            DFS(s + '(', stack + 1);
-        if (stack > 0)
-            DFS(s + ')', stack - 1);
+    vector<string> generateParenthesis(int n) {
+        vector<string> result;
+        dfs(n, "", 0, result);
+        return result;
     }
     
-    vector<string> generateParenthesis(int n) {
-        number = n;
-        
-        DFS("", 0);
-        
-        return result;
+  private:
+    void dfs(int total, string s, int open_left, vector<string>& result) {
+        if (s.length() == total * 2) {
+            if (open_left == 0) {
+                result.push_back(s);
+            }
+            return;
+        }
+        if (open_left < total) {
+            dfs(total, s + '(', open_left + 1, result);
+        }
+        if (open_left > 0) {
+            dfs(total, s + ')', open_left - 1, result);
+        }
     }
 };
