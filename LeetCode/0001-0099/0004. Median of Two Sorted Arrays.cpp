debc31fc3b776ee2874fcaf071f3800aa7f894af
cowtony <cowtony@163.com>
2021-11-19 10:55:29 -0800

    Update 0004. Median of Two Sorted Arrays.cpp

--- LeetCode/0001-0099/0004. Median of Two Sorted Arrays.cpp
+++ LeetCode/0001-0099/0004. Median of Two Sorted Arrays.cpp
@@ -1,39 +1,43 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int length = nums1.size() + nums2.size();
-        int k = length / 2;
+        if (nums1.size() > nums2.size()) {
+            swap(nums1, nums2);
+        }
+        
+        const int kNumsOnLeft = (nums1.size() + nums2.size()) / 2;  // Numbers to ignore (for the ODD length case).
+        int count_1, count_2;  // The `numbers to ignore` for the 2 vectors. The total should be equal to k.
+        int min_count_1 = 0, max_count_1 = nums1.size();
+        while (min_count_1 <= max_count_1) {
+            count_1 = (min_count_1 + max_count_1) / 2;
+            count_2 = kNumsOnLeft - count_1;
+            
+            if (count_2 < 0) {
+                max_count_1 = count_1 - 1;
+                continue;
+            } else if (count_2 > nums2.size()) {
+                min_count_1 = count_1 + 1;
+                continue;
+            }
+            
+            if (count_2 > 0 and count_1 < nums1.size() and nums1[count_1] < nums2[count_2 - 1]) {
+                min_count_1 = count_1 + 1;
+            } else if (count_1 > 0 and nums2[count_2] < nums1[count_1 - 1]) {
+                max_count_1 = count_1 - 1;
+            } else {
+                break;
+            }
+        }    
+        
+        int left_val = max(count_1 > 0? nums1[count_1 - 1] : INT_MIN, 
+                           count_2 > 0? nums2[count_2 - 1] : INT_MIN);
+        int right_val = min(count_1 < nums1.size()? nums1[count_1] : INT_MAX, 
+                            count_2 < nums2.size()? nums2[count_2] : INT_MAX);
 
-        if (length % 2 == 0) {
-            return (findKthNumber(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), k) + 
-                    findKthNumber(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), k + 1)) / 2.0;
+        if ((nums1.size() + nums2.size()) % 2 == 0) {
+            return (left_val + right_val) / 2.0;
         } else {
-            return findKthNumber(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), k + 1);
-        }
-    }
-    
-    int findKthNumber(const vector<int>::iterator& a_begin, 
-                      const vector<int>::iterator& a_end, 
-                      const vector<int>::iterator& b_begin, 
-                      const vector<int>::iterator& b_end, 
-                      int k) {
-        if (a_begin == a_end) {
-            return *(b_begin + k - 1);
-        }
-        if (b_begin == b_end) {
-            return *(a_begin + k - 1);
-        }
-        if (k == 1) {
-            return min(*a_begin, * b_begin);
-        }
-        
-        int a_val = (a_end - a_begin < k / 2) ? INT_MAX : *(a_begin + k / 2 - 1);
-        int b_val = (b_end - b_begin < k / 2) ? INT_MAX : *(b_begin + k / 2 - 1);
-        
-        if (a_val < b_val) {
-            return findKthNumber(a_begin + k / 2, a_end, b_begin, b_end, k - k / 2);
-        } else {
-            return findKthNumber(a_begin, a_end, b_begin + k / 2, b_end, k - k / 2);
+            return right_val;
         }
     }
 };
