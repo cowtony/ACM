b3bd93bf5282fd87dac2da80f771e76e731f817a
cowtony <cowtony@163.com>
2023-02-05 12:38:25 +0800

    Update binary_search.h

--- Notes/binary_search.h
+++ Notes/binary_search.h
@@ -37,4 +37,15 @@ return lo;
 /*
  * Binary search the answer:
  * LeetCode 2560: https://leetcode.com/problems/house-robber-iv/description/
+ * TODO:
+ * https://leetcode.com/problems/maximum-candies-allocated-to-k-children/description/
+ * https://leetcode.com/problems/maximum-value-at-a-given-index-in-a-bounded-array/description/
+ * https://leetcode.com/problems/kth-missing-positive-number/description/
+ * https://leetcode.com/problems/minimum-number-of-days-to-make-m-bouquets/description/
+ * https://leetcode.com/problems/find-the-smallest-divisor-given-a-threshold/description/
+ * https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/description/
+ * https://leetcode.com/problems/koko-eating-bananas/description/
+ * https://leetcode.com/problems/split-array-largest-sum/
+ * https://leetcode.com/problems/minimize-max-distance-to-gas-station/
+ * https://leetcode.com/problems/divide-chocolate/
  */
