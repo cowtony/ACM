fb8500952248b6f3d73010fd91d2ae4f1093c803
cowtony <cowtony@163.com>
2023-02-08 16:47:29 +0800

    Update string.h

--- Notes/string.h
+++ Notes/string.h
@@ -27,14 +27,26 @@ string join(const vector<string>& words, const string& delimiter = " ") {
 
 // KMP(Knuth-Morris-Pratt): Find string p in string s with O(s.len() + p.len()).
 // Longest prefix == suffix
+// Leetcode 28
 int KMP(const string& s, const string& p) {
-    vector<int> next = PMT(p);
-	for (int i = 0, j = 0; i < s.length(); i++, j++) {
-		if (s[i] != p[j]) {
-            
-		}
+    vector<int> next = PMT(needle);
+	for (int i = 0, j = 0; i < haystack.size(); i++) {
+	    while (haystack[i] != needle[j]) {
+            if (j > 0) {
+                j = next[j - 1];
+            } else {
+                j = -1;  // Offset the j++ later.
+                break;
+            }
+	    }
+	    j++;
+
+	    // Found a match.
+	    if (j == needle.length()) {
+		    return i - (j - 1);
+	    }
 	}
-	
+    return -1;
 }
 vector<int> PMT(const string& s) {
     vector<int> next(s.length(), 0);
