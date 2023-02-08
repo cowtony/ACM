b3d20f70c0779786e7cafa500350b8432ef2fbd5
cowtony <cowtony@163.com>
2023-02-08 15:54:17 +0800

    Create 1233. Remove Sub-Folders from the Filesystem.cpp

---
+++ LeetCode/1200-1299/1233. Remove Sub-Folders from the Filesystem.cpp
@@ -0,0 +1,68 @@
+template<class T = string>
+class Trie {
+  public:
+    ~Trie() {
+        for (auto [c, child] : children_) {
+            delete child;
+        }
+    }
+    
+    void insert(const string& word, const T& data = T()) {
+        Trie* node = this;
+        for (const char c : word) {
+            if (node->children_.find(c) == node->children_.end()) {
+                node->children_[c] = new Trie;
+            }
+            node = node->children_.at(c);
+            node->data_ = data;
+        }
+        node->word_ = word;
+    }
+    
+    T get(const string& word) const {
+        if (const Trie* node = getNode(word_)) {
+            return node->data_;
+        }
+        return T();
+    }
+
+    void getAllWords(vector<string>& result) {
+        if (!word_.empty()) {
+            result.push_back(word_); 
+        }
+        for (auto [c, node] : children_) {
+            if (word_.empty() || c != '/') {
+                node->getAllWords(result);
+            }
+        }
+    }
+    
+  private:
+    unordered_map<char, Trie*> children_;
+    string word_;
+    T data_;
+    
+    const Trie* getNode(const string& prefix) const {
+        const Trie* node = this;
+        for (char c : prefix) {
+            if (node->children_.find(c) == node->children_.end()) {
+                return nullptr;
+            }
+            node = node->children_.at(c);
+        }
+        return node;
+    }
+};
+
+class Solution {
+public:
+    vector<string> removeSubfolders(vector<string>& folder) {
+        Trie trie;
+        for (const string& s : folder) {
+            trie.insert(s);
+        }
+        vector<string> result;
+        trie.getAllWords(result);
+        return result;
+    }
+};
