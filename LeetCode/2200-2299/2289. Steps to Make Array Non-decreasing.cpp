53df7c609a7016b7ed96c36d6cecf4a716cf024b
cowtony <cowtony@163.com>
2023-02-11 09:47:25 +0800

    Update 2289. Steps to Make Array Non-decreasing.cpp

--- LeetCode/2200-2299/2289. Steps to Make Array Non-decreasing.cpp
+++ LeetCode/2200-2299/2289. Steps to Make Array Non-decreasing.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     int totalSteps(vector<int>& nums) {
         int result = 0;
-        vector<int> dp(nums.size());
+        vector<int> dp(nums.size(), 0);
         stack<int> monostack;  // mono decrease
         for (int i = nums.size() - 1; i >= 0; --i) {
             while (!monostack.empty() && nums[i] > nums[monostack.top()]) {
-                dp[i] = max(dp[i] + 1, dp[monostack.top()]);
+                dp[i]++;
+                dp[i] = max(dp[i], dp[monostack.top()]);
                 monostack.pop();
                 result = max(result, dp[i]);
             }
