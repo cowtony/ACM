d59de9fcc18cf97f5376c75ff5c03c1d84b0bdee
cowtony <cowtony@163.com>
2023-02-11 21:38:34 +0800

    Create 1081. Smallest Subsequence of Distinct Characters.cpp

---
+++ LeetCode/1000-1099/1081. Smallest Subsequence of Distinct Characters.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        vector<int> count(26, 0);
+        for (char c : s) {
+            count[c - 'a']++;
+        }
+        
+        vector<bool> visited(26, false);
+        string result;  // Also act as a monostack
+        for (char c : s) {
+            if (visited[c - 'a']) {
+                count[c - 'a']--;
+                continue;
+            }
+            while (!result.empty() && count[result.back() - 'a'] > 1 && c < result.back()) {
+                count[result.back() - 'a']--;
+                visited[result.back() - 'a'] = false;
+                result.pop_back();
+            }
+            result.push_back(c);
+            visited[c - 'a'] = true;
+        }
+        return result;
+    }
+};
