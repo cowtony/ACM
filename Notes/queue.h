d604af41a8f93f08af481463824917254ab1f24c
cowtony <cowtony@163.com>
2023-02-14 09:55:22 +0800

    Update queue.h

--- Notes/queue.h
+++ Notes/queue.h
@@ -1,4 +1,52 @@
+// Circular Buffer, producer - consumer
 template<class T>
 class Queue {
+  public:
+    Queue(int max_size) : kMaxSize(max_size), buffer(max_size) {
+        size_ = 0;
+        head_ = 0;
+        tail_ = 0;
+    }
+    
+    int size() const {
+        return size_;
+    }
+    
+    bool empty() const {
+        return size() == 0;
+    }
+    
+    std::pair<T, bool> top() const {
+        if (empty()) {
+            return {T(), false};
+        }
+        return {buffer.at(tail_), true};
+    }
+    
+    void pop() {
+        if (empty()) {
+            return;
+        }
+        tail_ = (tail_ + 1) % kMaxSize;
+        --size_;
+    }
+    
+    void push(const T&& data) {
+        if (size() == kMaxSize) {
+            pop();
+        }
+        buffer[head_] = std::move(data);
+        head_ = (head_ + 1) % kMaxSize;
+        ++size_;
+    }
+    
+    void resize(int new_size) {
+        // TODO
+    }
   
+  private:
+    const int kMaxSize;
+    vector<T> buffer;
+    int head_, tail_;
+    int size_;
 };
