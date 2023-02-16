59867e29556d6d237b882a226302aa0e12cba7ec
cowtony <cowtony@163.com>
2023-02-16 16:43:09 +0800

    Create 0474. Ones and Zeroes.cpp

---
+++ LeetCode/0400-0499/0474. Ones and Zeroes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+        for (const string& s : strs) {
+            int zero = count(s.begin(), s.end(), '0');
+            int one = s.length() - zero;
+            for (int i = m; i >= zero; i--) {
+                for (int j = n; j >= one; j--) {
+                    dp[i][j] = max(dp[i][j], dp[i - zero][j - one] + 1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
