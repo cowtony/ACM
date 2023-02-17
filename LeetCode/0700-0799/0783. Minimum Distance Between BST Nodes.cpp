8153a0a3c703bcef62f6bd678d3024b4036d3e61
cowtony <cowtony@163.com>
2023-02-17 10:03:32 +0800

    Create 0783. Minimum Distance Between BST Nodes.cpp

---
+++ LeetCode/0700-0799/0783. Minimum Distance Between BST Nodes.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDiffInBST(TreeNode* root) {
+        inOrderTraversal(root);
+        return diff;
+    }
+
+    void inOrderTraversal(TreeNode* node) {
+        if (!node) {
+            return;
+        }
+        inOrderTraversal(node->left);
+        diff = min(diff, abs(priv - node->val));
+        priv = node->val;
+        inOrderTraversal(node->right);
+    }
+
+    int diff = INT_MAX, priv = 10000;
+};
