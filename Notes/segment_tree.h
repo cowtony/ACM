e3a30b187022e3c4253866b2bcb471443e83371e
cowtony <cowtony@163.com>
2023-02-20 11:14:50 +0800

    Update segment_tree.h

--- Notes/segment_tree.h
+++ Notes/segment_tree.h
@@ -1,3 +1,82 @@
+// Segment tree with Lazy Propogation.
+class SegmentTree {
+public:
+    SegmentTree(int start, int end, const vector<int>& data) 
+      : kStart(start), kEnd(end) {
+        int size = 4 * (end - start + 1);
+        tree = vector<int>(size);
+        lazy = vector<int>(size);
+        build(/*root*/1, start, end, data);
+    }
+    
+    void update(int left, int right, int data) {
+        update(left, right, data, /*root*/1, kStart, kEnd);
+    }
+    int query(int left, int right) {
+        return query(left, right, /*root*/1, kStart, kEnd);
+    }
+
+private:
+    virtual int aggregate(int left, int right) const {
+        return left + right;     // Sum Tree
+        // return max(left, right); // Max tree
+        // return min(left, right); // Min tree
+    }
+
+    void build(int node, int start, int end, const vector<int>& data) {
+        if (start == end) {  // left node, string the single array element
+            tree[node] = data[start - kStart];
+            return;
+        }
+        int mid = (start + end) / 2;
+        build(node << 1, start, mid, data);
+        build(node << 1 | 1, mid + 1, end, data);
+        tree[node] = aggregate(tree[node << 1], tree[node << 1 | 1]);
+    }
+
+    void update(int left, int right, int data, int node, int start, int end) {
+        lazyPropogation(node, start, end);
+        if (end < left || start > right) { return; }
+        if (start >= left && end <= right) {
+            lazy[node]++;
+            lazyPropogation(node, start, end);
+            return;
+        }
+        int mid = (start + end) / 2;
+        update(left, right, data, node << 1, start, mid);
+        update(left, right, data, node << 1 | 1, mid + 1, end);
+        tree[node] = aggregate(tree[node << 1], tree[node << 1 | 1]);
+    }
+
+    int query(int left, int right, int node, int start, int end) {
+        lazyPropogation(node, start, end);
+        if (end < left  ||  start > right) { return 0; }
+        if (start >= left && end <= right) { return tree[node]; }
+        int mid = (start + end) / 2;
+        return aggregate(query(left, right, node << 1, start, mid),
+                         query(left, right, node << 1 | 1, mid + 1, end));
+    }
+
+    void lazyPropogation(int node, int start, int end) {
+        if (lazy[node] == 0) { return; }
+
+        // tree[node] += (end - start + 1) * lazy[node];
+        if(lazy[node] & 1)
+            tree[node] = (end - start + 1) - tree[node];
+
+        if (start != end) {  // mark both the child (if exist) lazy.
+            lazy[node << 1] += lazy[node];
+            lazy[node << 1 | 1] += lazy[node];
+        }
+        lazy[node] = 0;
+    }
+
+    const int kStart, kEnd;
+    vector<int> tree;  // Store the data for each tree node.
+    vector<int> lazy;  // lazy-propogation for range update.
+};
+
+
 template<class R = int, class T = int>  // Range and data.
 class SegmentTree {
 public:
