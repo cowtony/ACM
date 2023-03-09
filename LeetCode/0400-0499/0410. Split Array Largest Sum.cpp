c6cb1f5696ad28a8b34bdb68c5a030816b6ed2fe
cowtony <cowtony@163.com>
2023-03-09 20:43:42 +0800

    Create 0410. Split Array Largest Sum.cpp

---
+++ LeetCode/0400-0499/0410. Split Array Largest Sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int splitArray(vector<int>& nums, int k) {
+        int low = 0, high = INT_MAX;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (numOfSplits(nums, mid) > k) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+
+    int numOfSplits(const vector<int>& nums, int maximum) {
+        int count = 1;
+        int sum = 0;
+        for (int num : nums) {
+            if (num > maximum) {
+                return INT_MAX;
+            }
+            if (sum + num > maximum) {
+                count++;
+                sum = 0;
+            }
+            sum += num;
+        }
+        return count;
+    }
+};
