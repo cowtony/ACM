80e92774abf76753ffd4a025da86527da37c5d3b
cowtony <cowtony@163.com>
2023-03-13 09:45:04 +0800

    Create 2587. Rearrange Array to Maximize Prefix Score.cpp

---
+++ LeetCode/2500-2599/2587. Rearrange Array to Maximize Prefix Score.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxScore(vector<int>& nums) {
+        sort(nums.begin(), nums.end(), greater<int>());
+        long long sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            if (sum <= 0) {
+                return i;
+            }
+        }
+        return nums.size();
+    }
+};
