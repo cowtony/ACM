287b1a5ada2b795f27af2a15a609632af639db17
cowtony <cowtony@163.com>
2023-03-19 10:15:34 +0800

    Update 2591. Distribute Money to Maximum Children.cpp

--- LeetCode/2500-2599/2591. Distribute Money to Maximum Children.cpp
+++ LeetCode/2500-2599/2591. Distribute Money to Maximum Children.cpp
@@ -6,15 +6,16 @@ public:
         if (money < 0) {
             return -1;
         }
-        if (money / 7 > children) {
+        int result = money / 7;
+        if (result > children) {
             return children - 1;
         }
-        if (money / 7 == children && money % 7 != 0) {
-            return money / 7 - 1;
+        if (result == children && money % 7 != 0) {
+            return result - 1;
         }
-        if (money / 7 == children - 1 && money % 7 == 3) {
-            return money / 7 - 1;
+        if (result == children - 1 && money % 7 == 3) {
+            return result - 1;
         }
-        return money / 7;
+        return result;
     }
 };
