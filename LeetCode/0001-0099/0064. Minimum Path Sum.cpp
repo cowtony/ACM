6195d6677ba76ecc140a42493b8759c95934dcf9
cowtony <cowtony@163.com>
2023-03-26 20:57:49 -0700

    Update 0064. Minimum Path Sum.cpp

--- LeetCode/0001-0099/0064. Minimum Path Sum.cpp
+++ LeetCode/0001-0099/0064. Minimum Path Sum.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {        
-        for (int col = 1; col < grid[0].size(); col++)
-            grid[0][col] += grid[0][col - 1];
-        for (int row = 1; row < grid.size(); row++)
-            grid[row][0] += grid[row - 1][0];
-        
-        for (int row = 1; row < grid.size(); row++) {
-            for (int col = 1; col < grid[0].size(); col++) {
-                    grid[row][col] += min(grid[row - 1][col], grid[row][col - 1]);
+        for (int row = 0; row < grid.size(); row++) {
+            for (int col = 0; col < grid[0].size(); col++) {
+                if (row + col == 0) {
+                    continue;
+                }
+                grid[row][col] += min(row > 0? grid[row - 1][col] : INT_MAX, 
+                                      col > 0? grid[row][col - 1] : INT_MAX);
             }
         }
         return grid.back().back();
