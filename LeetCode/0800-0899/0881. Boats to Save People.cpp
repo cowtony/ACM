be5bacecd069555b414b8dcfa2f1235e4226ea1c
cowtony <cowtony@163.com>
2023-04-02 19:46:01 -0700

    Create 0881. Boats to Save People.cpp

---
+++ LeetCode/0800-0899/0881. Boats to Save People.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int left = 0, right = people.size() - 1;
+        int result = 0;
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                left++;
+            }
+            right--;
+            result++;
+        }
+        return result;
+    }
+};
