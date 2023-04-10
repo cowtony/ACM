0d490cb3a7e487f80dd3b691a5e9060ba4a12de4
cowtony <cowtony@163.com>
2023-04-09 18:31:16 -0700

    Update 0020. Valid Parentheses.cpp

--- LeetCode/0001-0099/0020. Valid Parentheses.cpp
+++ LeetCode/0001-0099/0020. Valid Parentheses.cpp
@@ -2,31 +2,13 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> sta;
-        for (char c: s)
-        {
-            switch (c)
-            {
-                case '(': sta.push('('); break;
-                case '[': sta.push('['); break;
-                case '{': sta.push('{'); break;
-                case ')': 
-                    if (!sta.empty() && sta.top() == '(')
-                        sta.pop();
-                    else
-                        return false;
-                    break;
-                case ']':
-                    if (!sta.empty() && sta.top() == '[')
-                        sta.pop();
-                    else
-                        return false;
-                    break;
-                case '}':
-                    if (!sta.empty() && sta.top() == '{')
-                        sta.pop();
-                    else
-                        return false;
-                    break;
+        for (char c: s) {
+            if (c == '(' || c == '[' || c == '{') {
+                sta.push(c);
+            } else if (!sta.empty() && c > sta.top() && c - sta.top() <= 2) {  // In ASCII the left and right bracket has at most 2 index away.
+                sta.pop();
+            } else {
+                return false;
             }
         }
         return sta.empty();
