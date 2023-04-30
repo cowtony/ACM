517891957f527b9bca2db13f5e76f5c9aa1832c9
cowtony <cowtony@163.com>
2023-04-29 18:44:10 -0700

    Update segment_tree.h

--- Notes/segment_tree.h
+++ Notes/segment_tree.h
@@ -1,6 +1,69 @@
+template<class T = int>  // Data.
+class SegmentTree {
+public:
+    SegmentTree(int start, int end, T default_val = T())
+      : kStart(start), kEnd(end), tree(4 * (end - start + 1)) {
+        build(1, start, end, default_val);
+    }
+
+    void update(int pos, T delta = T()) {
+        update(1, kStart, kEnd, pos, delta);
+    }
+
+    T query(int start, int end) {
+        return query(1, kStart, kEnd, start, end);
+    }
+
+private:
+    void build(int node, int start, int end, T default_val) {
+        if (start == end) {
+            tree[node] = default_val;
+            return;
+        }
+        int mid = (start + end) / 2;
+        build(node << 1, start, mid, default_val);
+        build(node << 1 | 1, mid + 1, end, default_val);
+        tree[node] = tree[node << 1] + tree[node << 1 | 1];
+    }
+
+    void update(int node, int start, int end, int pos, T delta) {
+        if (start == end) {
+            tree[node] += delta;
+            return;
+        }
+        int mid = (start + end) / 2;
+        if (pos <= mid) {
+            update(node << 1, start, mid, pos, delta);
+        } else {
+            update(node << 1 | 1, mid + 1, end, pos, delta);
+        }
+        tree[node] = tree[node << 1] + tree[node << 1 | 1];
+    }
+
+    T query(int node, int start, int end, int q_start, int q_end) {
+        if (start >= q_start && end <= q_end) {
+            return tree[node];
+        }
+        if (start > q_end || end < q_start) {
+            return T();
+        }
+        int mid = (start + end) / 2;
+        T left_query = query(node << 1, start, mid, q_start, q_end);
+        T right_query = query(node << 1 | 1, mid + 1, end, q_start, q_end);
+        return left_query + right_query;
+    }
+
+    const int kStart, kEnd;
+    vector<T> tree;
+};
+
+
 // Segment tree with Lazy Propogation.
 class SegmentTree {
 public:
+    SegmentTree(int start, int end, int default_val = 0)
+      : SegmentTree(start, end, vector<int>(end - start + 1, default_val)) {}
+      
     SegmentTree(int start, int end, const vector<int>& data) 
       : kStart(start), kEnd(end) {
         int size = 4 * (end - start + 1);
@@ -9,8 +72,8 @@ public:
         build(/*root*/1, start, end, data);
     }
     
-    void update(int left, int right, int data) {
-        update(left, right, data, /*root*/1, kStart, kEnd);
+    void update(int left, int right, int delta) {
+        update(left, right, delta, /*root*/1, kStart, kEnd);
     }
     int query(int left, int right) {
         return query(left, right, /*root*/1, kStart, kEnd);
@@ -34,17 +97,17 @@ private:
         tree[node] = aggregate(tree[node << 1], tree[node << 1 | 1]);
     }
 
-    void update(int left, int right, int data, int node, int start, int end) {
+    void update(int left, int right, int delta, int node, int start, int end) {
         lazyPropogation(node, start, end);
         if (end < left || start > right) { return; }
         if (start >= left && end <= right) {
-            lazy[node]++;
+            lazy[node] += delta;
             lazyPropogation(node, start, end);
             return;
         }
         int mid = (start + end) / 2;
-        update(left, right, data, node << 1, start, mid);
-        update(left, right, data, node << 1 | 1, mid + 1, end);
+        update(left, right, delta, node << 1, start, mid);
+        update(left, right, delta, node << 1 | 1, mid + 1, end);
         tree[node] = aggregate(tree[node << 1], tree[node << 1 | 1]);
     }
 
@@ -76,63 +139,3 @@ private:
     vector<int> lazy;  // lazy-propogation for range update.
 };
 
-
-template<class R = int, class T = int>  // Range and data.
-class SegmentTree {
-public:
-    SegmentTree(R start, R end, T data = T()) {
-        root = build(start, end, data);
-    }
-    void update(R pos, T delta = T()) { update(root, pos, delta); }
-    T query(R start, R end) { return query(root, start, end); }
-    
-private:
-    struct Node {
-        R start, end;
-        T data; // min, max, count, etc.
-        Node *left = nullptr, *right = nullptr;
-    }*root;
-    
-    static Node* build(R start, R end, T default_value) {
-        if (start > end) {
-            return nullptr;
-        }
-        Node *root = new Node{start, end, default_value};
-        if (start < end) {
-            root->left = build(start, start + (end - start) / 2, default_value);
-            root->right = build(start + (end - start) / 2 + 1, end, default_value);
-        }
-        return root;
-    }
-    
-    static void update(Node* root, R pos, T delta) {
-        if (!root) { return; }
-        if (pos < root->start or pos > root->end) {
-            return;
-        }
-        root->data += delta;  // Sum Tree.
-        if (!root->left and !root->right) {
-            return;
-        }
-        if (pos <= root->left->end) {
-            update(root->left, pos, delta);
-        } else if (pos >= root->right->start) {
-            update(root->right, pos, delta);
-        }
-    }
-    
-    static T query(Node* root, R start, R end) {
-        if (!root) { return T(); }
-        if (start <= root->start and end >= root->end) {
-            return root->data;
-        }
-        if (start > root->end or end < root->start) {
-            return T(); // Sum Tree.
-        }
-        return operation(query(root->left, start, end), query(root->right, start, end)); 
-    }
-    
-    static T operation(T left, T right) {
-        return left + right; // Sum Tree.
-    }
-};
