3e39015ee9a50d2360ab908cc71bd5d027ef5661
cowtony <cowtony@163.com>
2024-04-05 09:27:32 -0700

    Update 1483. Kth Ancestor of a Tree Node.cpp

--- LeetCode/1400-1499/1483. Kth Ancestor of a Tree Node.cpp
+++ LeetCode/1400-1499/1483. Kth Ancestor of a Tree Node.cpp
@@ -1,11 +1,12 @@
 // Binary Lifting
 class TreeAncestor {
 public:
-    TreeAncestor(int n, vector<int>& parent) :dp(n, vector<int>(17, -1)) {
+    TreeAncestor(int n, vector<int>& parent) : dp(n, vector<int>(kMaxPower, -1)) {
         for (int node = 0; node < n; node++) {
             dp[node][0] = parent[node];
         }
-        for (int power = 1; power < maxPower; power++) {
+        // dp[n][i] = dp[dp[n][i - 1]][i - 1]
+        for (int power = 1; power < kMaxPower; power++) {
             for (int node = 0; node < n; node++) {
                 if (dp[node][power - 1] != -1) {
                     dp[node][power] = dp[dp[node][power - 1]][power - 1];
@@ -15,12 +16,9 @@ public:
     }
     
     int getKthAncestor(int node, int k) { 
-        for (int power = maxPower, base = 1 << power; 
-             power >= 0 and k > 0; 
-             power--, base >>= 1) {
-            
+        for (int power = kMaxPower, base = 1 << power; power >= 0 && k > 0; power--, base >>= 1) {
             if (k >= base) {
-                if (node == -1 or power >= dp[node].size()) {
+                if (node == -1 || power >= dp[node].size()) {
                     return -1;
                 }
                 node = dp[node][power];
@@ -31,8 +29,7 @@ public:
     }
     
 private:
-    const int maxPower = 16;
-    // dp[n][i] = dp[dp[n][i - 1]][i - 1]
+    const int kMaxPower = 16;
     vector<vector<int>> dp; // dp[n][i] -> the 2^i th parent of node n.
 };
 
