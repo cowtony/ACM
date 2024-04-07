96e0366c2de15fc52fc578262a5d8c95646126f4
cowtony <cowtony@163.com>
2024-04-06 21:23:23 -0700

    Update 0678. Valid Parenthesis String.cpp

--- LeetCode/0600-0699/0678. Valid Parenthesis String.cpp
+++ LeetCode/0600-0699/0678. Valid Parenthesis String.cpp
@@ -3,28 +3,20 @@ public:
     bool checkValidString(string s) {
         int min_stack_size = 0, max_stack_size = 0;
         for (char c : s) {
-            switch (c) {
-                case '(':
-                    min_stack_size++;
-                    max_stack_size++;
-                    break;
-                case ')':
-                    if (min_stack_size > 0) {
-                        min_stack_size--;
-                    }
-                    max_stack_size--;
-                    if (max_stack_size < 0) {
-                        return false;
-                    }
-                    break;
-                case '*':
-                    if (min_stack_size > 0) {
-                        min_stack_size--;
-                    }
-                    max_stack_size++;
-                    break;
+            if (c == '(') {
+                min_stack_size++;
+                max_stack_size++;
+            } else if (c == ')') {
+                min_stack_size = max(min_stack_size - 1, 0);
+                max_stack_size--;
+            } else {
+                min_stack_size = max(min_stack_size - 1, 0);
+                max_stack_size++;
+            }
+            if (max_stack_size < 0) {
+                return false;
             }
         }
-        return min_stack_size <= 0 and max_stack_size >= 0;
+        return min_stack_size == 0;
     }
 };
