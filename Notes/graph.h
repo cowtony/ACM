38e41a6f517a66d319cece4d95b153b0ea21680a
cowtony <cowtony@163.com>
2024-04-20 20:48:25 -0700

    Update graph.h

--- Notes/graph.h
+++ Notes/graph.h
@@ -9,32 +9,6 @@ void floyd(vector<vector<int>>& adjacency_matrix) {
     }
 }
 
-// Dijkstra O((E+V)log(V))
-template<class V, class W>
-unordered_map<V, W> dijkstra(const unordered_map<V, vector<pair<W, V>>>& edges, int from) {
-    unordered_map<V, W> distance;
-    distance[from] = 0;
-    unordered_map<V, V> previous;
-
-    priority_queue<pair<W, V>, vector<pair<W, V>>, greater<pair<W, V>>> pq;
-    pq.push({0, from});
-    while (!pq.empty()) {
-        int next = pq.top().second;
-        pq.pop();
-
-        if (edges.find(next) == edges.end() or distance.find(next) == distance.end()) {
-            continue;
-        }
-        for (const auto& edge : edges.at(next)) {
-            if (distance.find(edge.second) == distance.end() or distance[edge.second] > distance[next] + edge.first) {
-                distance[edge.second] = distance[next] + edge.first;
-                pq.push(edge);
-            }
-        }
-    }
-    return distance;
-}
-
 // Check if graph has cycle
 bool hasCycle(const vector<vector<int>>& adjacency_list, vector<bool>& visited, int node = 0) {
     if (node < 0 or node >= visited.size()) {
