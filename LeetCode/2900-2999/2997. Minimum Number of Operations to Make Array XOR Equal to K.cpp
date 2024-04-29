b6b0c6479aa263ff530f16061506b43c395ea141
cowtony <cowtony@163.com>
2024-04-29 00:32:24 -0700

    Create 2997. Minimum Number of Operations to Make Array XOR Equal to K.cpp

---
+++ LeetCode/2900-2999/2997. Minimum Number of Operations to Make Array XOR Equal to K.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        for (int num : nums) {
+            k ^= num;
+        }
+        return __builtin_popcount(k);
+    }
+};
