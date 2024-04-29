ebe930d6463db94335f225d1e27c830b1a59eeec
cowtony <cowtony@163.com>
2024-04-29 09:18:30 -0700

    Create 2798. Number of Employees Who Met the Target.cpp

---
+++ LeetCode/2700-2799/2798. Number of Employees Who Met the Target.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+        return count_if(hours.begin(), hours.end(), [target](int hour) {
+            return hour >= target;
+        });
+    }
+};
