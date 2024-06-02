3da6581895f79e1603d0ea4df1b7f358da4d796d
Mu Niu <cowtony@163.com>
2024-06-02 15:32:53 +0800

    Update math.h

--- Notes/math.h
+++ Notes/math.h
@@ -1,12 +1,12 @@
 // Combination
-int64_t C(int total, int pick) {
-    if (pick > total) {
+uint64_t C(int total, int pick) {
+    if (total < 0 || pick < 0 || pick > total) {
         return 0;
     } else if (pick > total - pick) {
         return C(total, total - pick);
     }
 
-    int64_t result = 1;
+    uint64_t result = 1;
     for (int i = total, j = 1; j <= pick; --i, ++j) {
         result *= i;
         result /= j;
