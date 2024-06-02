2636254f07e92c69cdc6d02c015585a48fbfc045
Mu Niu <cowtony@163.com>
2024-06-02 15:33:38 +0800

    Create 2928. Distribute Candies Among Children I.cpp

---
+++ LeetCode/2900-2999/2928. Distribute Candies Among Children I.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int distributeCandies(int n, int limit) {
+        return C(n + 2, 2)  // Insert 2 boards to n + 2 slots, +2 to allow zero allocation.
+             - 3 * C(n + 2 - (limit + 1), 2)
+             + 3 * C(n + 2 - (limit + 1) * 2, 2)
+             - C(n + 2 - (limit + 1) * 3, 2);
+    }
+
+    uint64_t C(int total, int pick) {
+        if (total < 0 || pick < 0 || pick > total) {
+            return 0;
+        } else if (pick > total - pick) {
+            return C(total, total - pick);
+        }
+
+        uint64_t result = 1;
+        for (int i = total, j = 1; j <= pick; --i, ++j) {
+            result *= i;
+            result /= j;
+        }
+        return result;
+    }
+};
