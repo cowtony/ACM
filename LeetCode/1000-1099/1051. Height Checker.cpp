12574256baee7fc6aa5c4832497951d9c2d73196
Mu Niu <cowtony@163.com>
2024-06-10 08:26:16 +0800

    Create 1051. Height Checker.cpp

---
+++ LeetCode/1000-1099/1051. Height Checker.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        auto expected = heights;
+        sort(expected.begin(), expected.end());
+
+        int result = 0;
+        for (int i = 0; i < heights.size(); i++) {
+            if (heights[i] != expected[i]) {
+                result++;
+            }
+        }
+        return result;
+    }
+};
