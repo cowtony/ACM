b314aec3faef9f135aebcdcfb237802a9923f626
Mu Niu <cowtony@163.com>
2024-06-20 14:41:25 +0800

    Update math.h

--- Notes/math.h
+++ Notes/math.h
@@ -13,3 +13,21 @@ uint64_t C(int total, int pick) {
     }
     return result;
 }
+
+// Greatest Common Divisor
+int gcd(int a, int b) {
+    if (a == 0) { return b; }
+    return gcd(b % a, a);
+}
+
+// Check if it's prime number
+bool isPrime(int n) {
+    if (n < 2) { return false; }
+    if (n % 2 == 0) { return n == 2; }
+    for (int i = 3; i * i <= n; i += 2) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
