d13cce313b832e7260d887ec97d280ff1867ef33
Mu Niu <cowtony@163.com>
2024-07-31 20:12:27 -0700

    Create 2678. Number of Senior Citizens.cpp

---
+++ LeetCode/2600-2699/2678. Number of Senior Citizens.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int count = 0;
+        for (const string& detail : details) {
+            int age = stoi(detail.substr(11, 2));
+            if (age > 60) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
