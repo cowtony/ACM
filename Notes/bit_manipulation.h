b535e41afbb425d9d5baaa1739f7a07f19594f36
Mu Niu <cowtony@163.com>
2024-08-20 14:27:44 -0700

    Update bit_manipulation.h

--- Notes/bit_manipulation.h
+++ Notes/bit_manipulation.h
@@ -22,5 +22,7 @@ void toggleBit(int& num, int k) {
 std::bitset<10> x;
 
 long long countIthBitsUnder(long long num, int i) {
+    // Example: the third bit, 100 -> 4
+    // return num / 8 * 4 + max(0, num % 8 - 4)
     return (num >> i + 1 << i) + max(0LL, (num & (1LL << i + 1) - 1) - (1LL << i));
 }
